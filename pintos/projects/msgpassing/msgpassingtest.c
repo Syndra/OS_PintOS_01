8275c52f68d13672c5e81a709d01ef7a2fba7fe1
Lenny <lenny@Lennyui-MacBook-Pro.local>
2018-03-31 23:34:21 +0900

    commited!

---
+++ pintos/projects/msgpassing/msgpassingtest.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+#include "projects/msgpassing/msgpassingtest.h"
+#include "threads/synch.h"
+
+void run_message_passing_test(void)
+{
+	printf("implement msgpassing and its tests !!!\n");
+	message_test();
+}
