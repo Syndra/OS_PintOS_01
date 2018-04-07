efd4ec87552a08017e98a1f809b273e4f5e51a20
Lenny <lenny@Lennyui-MacBook-Pro.local>
2018-04-07 15:12:05 +0900

    출력 동기화면 하면 됨

--- projects/crossroads/crossroads.c
+++ projects/crossroads/crossroads.c
@@ -11,13 +11,12 @@
 #include "projects/crossroads/crossroads.h"
 #include "projects/crossroads/mapdata.h"
 
-struct semaphore control[6];
 struct semaphore s;
-struct semaphore print;
+struct semaphore control[4];
 int numberofcar;
 int print_turn;
 char map_draw[7][7];
-int *process_status;
+int car_location[7][7];
 
 static void car_thread(void);
 
@@ -35,16 +34,16 @@ run_crossroads(char **argv)
 	ASSERT(argv != NULL);
 
 	//semaphore initialize.
-	sema_init(&s, 1);
-	sema_init(&print, 1);
+  sema_init(&s, 1);
 	sema_init(&control[0], 1);
 	sema_init(&control[1], 1);
 	sema_init(&control[2], 1);
 	sema_init(&control[3], 1);
 
 	//initialize.
-	input = (char *)malloc(sizeof(argv[1])*4);
-	strlcpy(input, argv[1], sizeof(argv[1])*4);
+	//input = (char *)malloc(sizeof(strlen(argv[1]));
+	//strlcpy(input, argv[1], sizeof(strlen(argv[1]));
+  input = argv[1];
 
   printf("input string : %s ", input);
 
@@ -53,7 +52,6 @@ run_crossroads(char **argv)
   printf("lenght : %d number of car : %d\n", strlen(input), numberofcar);
 
 	print_turn = numberofcar;
-	process_status = (int *)malloc(sizeof(int)*numberofcar);
   car_data = (char **)malloc(sizeof(4));
 
 	for(i = 0; i < 7; i++)
@@ -61,9 +59,9 @@ run_crossroads(char **argv)
 		for(j = 0; j < 7; j++)
 		{
 			map_draw[i][j] = map_draw_default[i][j];
+      car_location[i][j] =  0;
 		}
 	}
-  printf("/////////////////////////////\n");
 	//tokenize argv with token " : " .
 	i = 0;
 	for (token = strtok_r(input, ":", &save_ptr); token != NULL; token = strtok_r (NULL, ":", &save_ptr))
@@ -91,32 +89,59 @@ print_mapdata(void)
 			for(j = 0; j < 7; j++)
 			{
 				printf("%c", map_draw[i][j]);
+        //printf("%d", car_location[i][j]);
 			}
 			printf("\n");
 	}
+  // for(i = 0;i<17;i++)
+  // {
+  // printf("\n");
+  // }
+    //timer_mdelay(300);
+}
+
+int
+set_move(int source_num, int dest_num, int status, char car_name)
+{
+	struct position *move_position;
+	struct position *current_position;
+	move_position = &path[source_num][dest_num][status];
+
+  if(move_position->row == -1)
+  {
+    current_position = &path[source_num][dest_num][status - 1];
+    map_draw[current_position->row][current_position->col] = map_draw_default[current_position->row][current_position->col];
+    car_location[current_position->row][current_position->col] = 0;
+    return 40;
+  }
+
+  else if(is_occupied(move_position->row, move_position->col))
+  {
+    return status;
+  }
+
+  else
+  {
+    map_draw[move_position->row][move_position->col] = car_name;
+    car_location[move_position->row][move_position->col] = 1;
+    if(status != 0)
+    {
+      current_position = &path[source_num][dest_num][status - 1];
+      map_draw[current_position->row][current_position->col] = map_draw_default[current_position->row][current_position->col];
+      car_location[current_position->row][current_position->col] = 0;
+    }
+    status++;
+    return status;
+  }
 }
 
 bool
-set_move(int source_num, int dest_num, int status, char car_name)
+is_occupied(int row, int col)
 {
-	struct position move_position;
-	struct position prev_position;
-	move_position = path[source_num][dest_num][status];
-	if(move_position.row != -1)
-	{
-		map_draw[move_position.col][move_position.row] = car_name;
-
-		if(status != 0)
-		{
-			prev_position = path[source_num][dest_num][status - 1];
-			map_draw[prev_position.col][prev_position.row] = map_draw_default[prev_position.col][prev_position.row];
-		}
-    return false;
-	}
-  else
-  {
+  if(car_location[row][col] == 1)
     return true;
-  }
+  else
+    return false;
 }
 
 /* car_info is implemented like string "aAB", or "bBC" from thread name.
@@ -131,12 +156,10 @@ static void
 car_thread(void) {
 
 	int process_status = 0;
-  int loop = 10;
 	char * thread_name_;
 	char car_name;
 	char source;
 	char destination;
-  bool isfinished  = false;
 
 	thread_name_ = (char *)malloc(sizeof(thread_name()));
 	strlcpy(thread_name_, thread_name(), sizeof(thread_name()));
@@ -145,9 +168,9 @@ car_thread(void) {
 	source = thread_name_[1];
 	destination = thread_name_[2];
 
-  while(loop > 0){
-	sema_down(&s);
+  printf("thread init %c:%c:%c\n", car_name, source, destination);
 
+  while(true){
 	if(source == 'A' && destination == 'B')
 	{
 		if(process_status == 2)
@@ -160,8 +183,7 @@ car_thread(void) {
 			sema_up(&control[2]);
       printf("out of critical section! A-B\n");
 		}
-		//isfinished = set_move(1, 2, process_status, car_name);
-    process_status++;
+		process_status = set_move(0, 1, process_status, car_name);
     printf("thread : [%s] car name : %c status : %d\n", thread_name(), car_name, process_status);
   }
 
@@ -179,8 +201,7 @@ car_thread(void) {
 			sema_up(&control[2]);
       printf("out of critical section! A-C\n");
 		}
-		//isfinished = set_move(1, 3, process_status, car_name);
-    process_status++;
+		process_status = set_move(0, 2, process_status, car_name);
     printf("thread : [%s] car name : %c status : %d\n", thread_name(), car_name, process_status);
   }
 
@@ -200,8 +221,7 @@ car_thread(void) {
 			sema_up(&control[0]);
       printf("out of critical section! B-A\n");
 		}
-		//isfinished = set_move(2, 1, process_status, car_name);
-    process_status++;
+		process_status = set_move(1, 0, process_status, car_name);
     printf("thread : [%s] car name : %c status : %d\n", thread_name(), car_name, process_status);
   }
 
@@ -217,8 +237,7 @@ car_thread(void) {
 			sema_up(&control[3]);
       printf("out of critical section! B-C\n");
 		}
-		//isfinished = set_move(2, 3, process_status, car_name);
-    process_status++;
+		process_status = set_move(1, 2, process_status, car_name);
     printf("thread : [%s] car name : %c status : %d\n", thread_name(), car_name, process_status);
   }
 
@@ -236,8 +255,7 @@ car_thread(void) {
 			sema_up(&control[0]);
       printf("out of critical section! C-A\n");
 		}
-		//isfinished = set_move(3, 1, process_status, car_name);
-    process_status++;
+		process_status = set_move(2, 0, process_status, car_name);
     printf("thread : [%s] car name : %c status : %d\n", thread_name(), car_name, process_status);
   }
 
@@ -257,29 +275,23 @@ car_thread(void) {
 			   sema_up(&control[0]);
          printf("out of critical section! C-B\n");
 		  }
-		//isfinished = set_move(3, 2, process_status, car_name);
-    process_status++;
+		process_status = set_move(2, 1, process_status, car_name);
     printf("thread : [%s] car name : %c status : %d\n", thread_name(), car_name, process_status);
   }
 
-	sema_down(&print);
-
-	print_turn--;
-
-	if(print_turn == 0){
+  print_turn--;
+	if(print_turn == 0)
+  {
 		print_mapdata();
 		print_turn = numberofcar;
 	}
 
-	sema_up(&print);
-
-  sema_up(&s);
-  // if(isfinished)
-  // {
-  //   printf("thread [%s] is finished!\n", thread_name());
-  //   break;
-  // }
-  loop--;
-  timer_mdelay(40);
+  if(process_status > 39)
+  {
+    printf("thread [%s] is finished!\n", thread_name());
+    numberofcar--;
+    break;
+  }
+  timer_sleep(1);
 }
 }
--- projects/crossroads/crossroads.h
+++ projects/crossroads/crossroads.h
@@ -4,6 +4,7 @@
 
 void run_crossroads(char **argv);
 void print_mapdata(void);
-bool set_move(int, int, int, char);
+int set_move(int, int, int, char);
+bool is_occupied(int, int);
 
 #endif /* __PROJECTS_PROJECT1_CROASSROADS_H__ */
